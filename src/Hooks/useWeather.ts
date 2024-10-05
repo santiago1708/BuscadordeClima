0da4847f10e85818e93329ab22f2c1bf14511ac6
santiago1708 <vela123tique@gmail.com>
2024-10-05 11:17:26 -0500

    Se agrego valibot por Zod, esto fue una prueba de esta libreria sin embargo se regrasara a Zod

--- src/Hooks/useWeather.ts
+++ src/Hooks/useWeather.ts
@@ -1,18 +1,18 @@
 import axios from "axios"
-import {z} from 'zod'
+import {object, string, number, InferOutput, parse} from 'valibot'
 import { SearchType } from "../types"
 
 //ZOD
-const Weather = z.object({
-    name: z.string(),
-    main: z.object({
-        temp: z.number(),
-        temp_max: z.number(),
-        temp_min: z.number(),
+const WeatherSchema = object({
+    name: string(),
+    main: object({
+        temp: number(),
+        temp_max: number(),
+        temp_min: number(),
     })
 })
 
-type Weather = z.infer<typeof Weather>
+type Weather = InferOutput<typeof WeatherSchema>
 
 
 export default function useWeather() {
@@ -31,11 +31,11 @@ export default function useWeather() {
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}` //segundo llamado
             const {data : weatherData} = await axios(weatherUrl) //{data : weatherData} esto renombra el nombre del data a weatherData
             
-            //Zod
-            const result = Weather.safeParse(weatherData)
-            if(result.success) {
-                console.log(result.data.name)
-                console.log(result.data.main.temp)
+            //Valibot
+            const result = parse(WeatherSchema, weatherData)
+            if(result) {
+                console.log(result.name)
+                console.log(result.main.temp)
             }else {
                 console.log('La consulta fallo')
             }
