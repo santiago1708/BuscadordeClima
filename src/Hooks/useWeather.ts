4424c38b6c6ffd262f21029ec96564a0b7d3f92e
santiago1708 <vela123tique@gmail.com>
2024-10-05 10:01:26 -0500

    Se reemplazo el Type Guard por Zod

--- src/Hooks/useWeather.ts
+++ src/Hooks/useWeather.ts
@@ -1,19 +1,18 @@
-import { Weather } from './../types/index';
 import axios from "axios"
+import {z} from 'zod'
 import { SearchType } from "../types"
 
-// Esta función valida si la respuesta del API de OpenWeatherMap es un objeto con los datos esperados.
-//Tipar el objeto esperado de la api para un autocompleato
-function isWeatherResponse(weather : unknown)  {
-    return(
-        Boolean(weather) && 
-        typeof weather === 'object' && 
-        typeof (weather as Weather).name === 'string' && 
-        typeof (weather as Weather).main.temp === 'number' && 
-        typeof (weather as Weather).main.temp_max === 'number' && 
-        typeof (weather as Weather).main.temp_min === 'number' 
-    )
-}
+//ZOD
+const Weather = z.object({
+    name: z.string(),
+    main: z.object({
+        temp: z.number(),
+        temp_max: z.number(),
+        temp_min: z.number(),
+    })
+})
+
+type Weather = z.infer<typeof Weather>
 
 
 export default function useWeather() {
@@ -31,10 +30,15 @@ export default function useWeather() {
 
             const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}` //segundo llamado
             const {data : weatherData} = await axios(weatherUrl) //{data : weatherData} esto renombra el nombre del data a weatherData
-        
-            const result = isWeatherResponse(weatherData)
-            console.log(result)
-
+            
+            //Zod
+            const result = Weather.safeParse(weatherData)
+            if(result.success) {
+                console.log(result.data.name)
+                console.log(result.data.main.temp)
+            }else {
+                console.log('La consulta fallo')
+            }
 
         } catch (error) {
             console.log(error)
